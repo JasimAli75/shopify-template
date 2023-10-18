2ef9eaeddfb334d20a4698861ba536d8c758e072
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-10-19 04:38:45 +0500

    shancn/UI added

---
+++ src/BodyComponents/Views/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col gap-10">
+      Shopify template project
+      <Button>Click the shopify project</Button>
+    </div>
+  );
+};
+
+export default Header;
