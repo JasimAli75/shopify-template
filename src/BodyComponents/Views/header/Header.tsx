257e1fc5f50ea8d57f8823df69874a6ecc59dc90
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-12-03 04:27:06 +0500

    test

--- src/BodyComponents/Views/header/Header.tsx
+++ src/BodyComponents/Views/header/Header.tsx
@@ -7,16 +7,6 @@ const Header = () => {
       Shopify template project
       <Button>Click the shopify project</Button>
       <p>Jasim Ali Rajput new tempalte for project </p>
-      <p>this is new power of AI generative AI boss </p>
-      <h1>
-        Th is about new beanch rule and other administraor rules for work in
-        team
-      </h1>
-      <h2>
-        THis is new branceh test and you can easily tell me about this branch
-        team thanks
-      </h2>
-      <h2>new work added</h2>
     </div>
   );
 };
